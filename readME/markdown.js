a9c988a1961945abdcd99f6703d2732ba0531447
Jose <josejrod07@gmail.com>
2023-05-15 13:42:08 -0400

    add markdown.js changes

--- readME/markdown.js
+++ readME/markdown.js
@@ -2,7 +2,7 @@
 // If there is no license, return an empty string
 function licenseBadge(license) {
     if (license !== 'None') {
-      return `![GitHub License](https://img.shields.io/badge/license-${license}-blue.svg)`;
+      return `![GitHub License](https://img.shields.io/badge/License-${license}-blue.svg)`;
     }
     return '';
   }
